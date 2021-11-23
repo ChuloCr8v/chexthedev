4f88347d5c6b1cba763fc6fb0fc80766a2d16501
chulocr8v <nkematu5@gmail.com>
2021-11-23 19:11:39 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import {FaHome} from 'react-icons/fa'
+import {
+  FaHome
+} from 'react-icons/fa'
 import styles from '../styles/Index.module.scss'
 import HeroSection from '../comps/HeroSection'
 import About from '../comps/About'
@@ -8,12 +10,14 @@ import ContactSection from '../comps/ContactSection'
 
 export default function Index() {
   return (
-  <div>
-   <HeroSection />
-   <About />
-   <Skills />
-   <Portfolio />
-   <ContactSection />
-  </div>
+    <div className={styles.main}>
+      <div className={styles.container}>
+      <HeroSection />
+      <About />
+      <Skills />
+      <Portfolio />
+      <ContactSection />
+    </div>
+    </div>
   )
 }
